c04e6d91ba0d47929a14e802d45a7d7adf143e7d
Gabriella Apeadu <apeadug@gmail.com>
2021-10-04 09:13:24 -0400

    fixed file paths to assets

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -66,34 +66,34 @@ export default class Preloader extends Phaser.Scene {
     this.timedEvent = this.time.delayedCall(3000, this.ready, [], this);
 
     //load assets needed in game
-    this.load.image('tiles', './public/assets/SmoothdungeonTileset.png');
-    this.load.tilemapTiledJSON('dungeon', 'assets/dungeon_1.json');
+    this.load.image('tiles', '../public/assets/SmoothdungeonTileset.png');
+    this.load.tilemapTiledJSON('dungeon', '../public/assets/dungeon_1.json');
 
-    this.load.image('blueButton1', './public/assets/ui/blue_button01.png');
-    this.load.image('blueButton2', './public/assets/ui/blue_button02.png');
-    this.load.image('box', './public/assets/ui/grey_box.png');
-    this.load.image('checkedBox', './public/assets/ui/blue_boxCheckmark.png');
+    this.load.image('blueButton1', '../public/assets/ui/blue_button01.png');
+    this.load.image('blueButton2', '../public/assets/ui/blue_button02.png');
+    this.load.image('box', '../public/assets/ui/grey_box.png');
+    this.load.image('checkedBox', '../public/assets/ui/blue_boxCheckmark.png');
 
-    this.load.image('darkCastle', './public/assets/darkCastle.png');
+    this.load.image('darkCastle', '../public/assets/darkCastle.png');
 
     //load textured spritesheet
     this.load.atlas(
       'warrior',
-      'spriteSheet/warrior.png',
-      'spriteSheet/warrior.json'
+      '../public/spriteSheet/warrior.png',
+      '../public/spriteSheet/warrior.json'
     );
 
     this.load.atlas(
       'wizzard',
-      'spriteSheet/enemy/wizzard.png',
-      'spriteSheet/enemy/wizzard.json'
+      '../public/spriteSheet/enemy/wizzard.png',
+      '../public/spriteSheet/enemy/wizzard.json'
     );
   } //end of preload
 
   ready() {
     this.readyCount++;
     if (this.readyCount === 2) {
-      this.scene.start('title');
+      this.scene.start('game');
     }
   }
 }
