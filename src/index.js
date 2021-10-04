23d5bfa93a46d92cf264f0a8762df9a27af91444
Gabriella Apeadu <apeadug@gmail.com>
2021-10-04 09:19:08 -0400

    cleaned up code

--- src/index.js
+++ src/index.js
@@ -19,25 +19,3 @@ class Game extends Phaser.Game {
 window.onload = function () {
   window.game = new Game();
 };
-
-// export const config = {
-//   type: Phaser.AUTO,
-//   backgroundColor: 'rgba(158, 42, 0, 0.5)',
-//   width: 800,
-//   height: 450,
-
-//   physics: {
-//     default: 'arcade',
-//     arcade: {
-//       debug: true,
-//       gravity: { y: 0 },
-//     },
-//   },
-//   scene: [Preloader, TitleScene, Game],
-//   // scale: {
-//   //   parent: 'mygame',
-//   //   mode: Phaser.Scale.FIT,
-
-//   //   // zoom: 1.5,
-//   // },
-// };
